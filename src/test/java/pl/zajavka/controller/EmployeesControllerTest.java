39f319a1628f3e8178fb63d7bc4f51dae32501b5
WaldemarGoszka <105630599+WaldemarGoszka@users.noreply.github.com>
2023-05-11 23:38:51 +0200

    add simple unit test

--- src/test/java/pl/zajavka/controller/EmployeesControllerTest.java
+++ src/test/java/pl/zajavka/controller/EmployeesControllerTest.java
@@ -1,6 +1,6 @@
 package pl.zajavka.controller;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @ExtendWith(MockitoExtension.class)
-class EmployeesControllerTest {
+public class EmployeesControllerTest {
 
     @Mock
     private EmployeeRepository employeeRepository;
